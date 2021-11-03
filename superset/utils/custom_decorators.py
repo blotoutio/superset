9e63245a5543e3541db8704c23e5597ab80785cc
tajmulhussainr <tajmulhussain.r@gmail.com>
2021-11-03 05:50:21 -0700

    added json interceptor functionality

--- superset/utils/custom_decorators.py
+++ superset/utils/custom_decorators.py
@@ -6,18 +6,32 @@ from werkzeug.exceptions import abort
 from superset.utils.permissions_manager import PermissionsManager
 
 
-def authenticate_permissions_request(is_sql_query):
+def authenticate_permissions_request(is_sql_query=False, is_json_query=False):
 
     def wrap(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
+            def check_sql_permission(sql):
+                permissions = PermissionsManager()
+                return permissions.check_for_sql(sql)
 
             if is_sql_query:
                 sql = request.json['sql']
-                permissions = PermissionsManager()
-                res = permissions.check_for_sql(sql)
+                res = check_sql_permission(sql)
                 if not res[0]:
                     return abort(401, res[1])
+            
+            if is_json_query:
+                json_resp = fn(*args, **kwargs)
+                if json_resp:
+                    # '\n' in response causes exception during parsing. So replacing new line escape sequence
+                    json_resp.replace("\n", " ")
+                    resp = json.loads(json_resp)
+                    for cacheObj in resp["result"]:
+                        sql = cacheObj["query"]
+                        res = check_sql_permission(sql)
+                        if not res[0]:
+                            return abort(401, res[1])
 
             return fn(*args, **kwargs)
 
